658d0c6716ff03da0aa710d1406d1c2f0dfb1f2b
Tomomi-Ichihashi <ichihashi.1414.tomomi@gmail.com>
2021-05-17 05:26:39 +0000

    [Add]ヘッダー・フッダー

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -5,13 +5,13 @@ class Users::ConfirmationsController < ApplicationController
     # confirmation.user_id = current_user.idが済んだ状態で生成
     confirmation = current_user.confirmations.build(report_id: params[:report_id])
     confirmation.save
-    redirect_back(fallback_location:root_path)
+    # redirect_back(fallback_location:root_path)
   end
 
   def destroy
     report = Report.find(params[:report_id])
     confirmation = current_user.confirmations.find_by(report_id: params[:report_id])
     confirmation.destroy
-    redirect_back(fallback_location:root_path)
+    # redirect_back(fallback_location:root_path)
   end
 end
